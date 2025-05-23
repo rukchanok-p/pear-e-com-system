462d505048a1f3f1f4f4d99b3bca703f3205b189
rukchanok <rukchanok.p@ntplc.co.th>
2025-05-23 13:40:10 +0700

    deplo

--- src/App.tsx
+++ src/App.tsx
@@ -16,8 +16,8 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>E-Commerce System test build</h1>
-      <h1>Test Workflow V7</h1>
+      <h1>E-Commerce System test build V8.0.0</h1>
+      <h1>Test Workflow</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
