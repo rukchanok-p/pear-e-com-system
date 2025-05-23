3f077994d26c74353f87355e377d4e79886f2241
rukchanok <rukchanok.p@ntplc.co.th>
2025-05-23 10:43:39 +0700

    test error

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>E-Commerce System</h1>
+      <h1>E-Commerce System</h1
       <h1>Test Workflow V7</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
